3819eac6b1df234895f3743b7f028060c5d7dc39
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-09-20 16:09:22 -0300

    initial commit

---
+++ src/test/java/com/assessment/infnet/InfnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.infnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
